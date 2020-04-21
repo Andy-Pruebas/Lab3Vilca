33fc3ced5909f899d16fe3f5e8a9845c89e8796b
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-04-20 21:49:03 -0500

    first commit

---
+++ Lab3V/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab3V
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new ScrollViewDemo();
+            //MainPage = new GridDemo();
+            //MainPage = new RelativeLayoutDemo();
+            //MainPage = new AbsoluteLayoutDemo();
+            //MainPage = new StackLayoutDemo();
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
